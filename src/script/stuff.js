54da0b253c2672866a1804b1328dab5ab37efd64
WhySoVerySirius <vytartast@gmail.com>
2022-01-18 16:20:41 +0200

    toggle

--- src/script/stuff.js
+++ src/script/stuff.js
@@ -2,34 +2,38 @@ $(document).ready(function() {
     $('#switch').on('click', function() {
         if ($('nav').css('display') == 'flex') {
             $('nav').css('display', 'none');
-            $('.fa-times').css('display', 'none');
-            $('.fa-bars').css('display', 'block')
+            $('.fa-times').toggle();
+            $('.fa-bars').toggle();
         } else {
             $('nav').css('display', 'flex')
-            $('.fa-times').css('display', 'block');
-            $('.fa-bars').css('display', 'none')
+            $('.fa-times').toggle();
+            $('.fa-bars').toggle();
         }
     });
+    // navigacijos paslepimas
     $(document).click(function(navhide) {
         let container = $(".header");
         if (!container.is(navhide.target) && container.has(navhide.target).length === 0) {
             $('nav').hide();
             $('.fa-times').css('display', 'none');
-            $('.fa-bars').css('display', 'block')
+            $('.fa-bars').css('display', 'block');
         }
     });
+    // filtro atidarymas
     $('.filterExpand').on('click', function() {
-        $('.buttons').show();
-        $('.filterExpand').hide()
+        $('.buttons').toggle();
+        $('.filterExpand').toggle();
     });
+    // filtro paslepimas
     $(document).click(function(filterhide) {
-        let container2 = $('.filter');
-        if (!container2.is(filterhide.target) && container2.has(filterhide.target).length === 0) {
-            $('.buttons').hide();
-            $('.filterExpand').show();
-            $('.bannerContainer').children().show();
-        }
-    })
+            let container2 = $('.filter');
+            if (!container2.is(filterhide.target) && container2.has(filterhide.target).length === 0) {
+                $('.buttons').hide();
+                $('.filterExpand').show();
+                $('.bannerContainer').children().show();
+            }
+        })
+        // filtras
     $('#white').on('click', function() {
         $('.img.not_white').hide();
         $('.img.white').show()
